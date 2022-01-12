93a9f1ed22d6eaee0fe5153076262604e53cf5f8
Andrei Drexler <makro@rq3.com>
2022-01-12 15:06:39 +0200

    Use lowercase header name to fix cross-compilation error
    Note: the file *is* capitalized as "VersionHelpers.h" in the Visual Studio platform SDK

--- Quake/sys_sdl_win.c
+++ Quake/sys_sdl_win.c
@@ -26,7 +26,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #endif
 #include <windows.h>
 #include <mmsystem.h>
-#include <VersionHelpers.h>
+#include <versionhelpers.h>
 
 #include "quakedef.h"
 
